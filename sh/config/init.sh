37d0de1ceec58c5f2306c81cc172dfe81a7053f3
maany <imptodefeat@gmail.com>
2019-10-23 15:18:45 +0200

    update init.sh for pre_config and config

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "LOLO"
+echo "----------------------------------"
+echo "Initializing HTCondor SCHEDD"
+echo "----------------------------------"
+cp $SIMPLE_CONFIG_DIR/config/50_PC.conf $HTCONDOR_CONFIG_DIR/config.d/50PC.conf
+
+
+echo "----------------------------------"
+echo "Starting daemons"
+echo "----------------------------------"
+echo "Starting HTCondor"
+systemctl start condor
+echo "Starting crond"
+systemctl start crond
+
+echo "Initialization Complete!"
